46c2e72c12af80c3c62bbe9a6b2254a1770fdc74
mberndt92 <maximilian.berndt@foodora.com>
2018-03-14 14:02:32 +0100

    Initial Commit

---
+++ BridgingFramework/BridgingFramework.h
@@ -0,0 +1,19 @@
+//
+//  BridgingFramework.h
+//  BridgingFramework
+//
+//  Created by Berndt, Maximilian on 14.03.18.
+//  Copyright © 2018 mb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BridgingFramework.
+FOUNDATION_EXPORT double BridgingFrameworkVersionNumber;
+
+//! Project version string for BridgingFramework.
+FOUNDATION_EXPORT const unsigned char BridgingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BridgingFramework/PublicHeader.h>
+
+
